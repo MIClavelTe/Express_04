d85d67c7f8e1fc99f3bc1254f47c490001b57d0e
MIClavelTe <miclavelte@gmail.com>
2018-08-01 11:17:51 -0700

    Add error handler

--- app.js
+++ app.js
@@ -53,6 +53,12 @@ app.post('/bye', (req, res) => {
   res.redirect('/student');
 });
 
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
 app.use((err, req, res, next) => {
   res.locals.error = err;
   res.status(err.status);
